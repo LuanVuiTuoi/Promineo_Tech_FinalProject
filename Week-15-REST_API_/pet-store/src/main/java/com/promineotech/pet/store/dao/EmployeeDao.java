fe9f9870c108c311e71fd30e5e67845de987d4b6
Luan Nguyen <43247207+LuanVuiTuoi@users.noreply.github.com>
2024-12-30 12:42:00 -0700

    Delete Week-15-REST_API_ directory

--- Week-15-REST_API_/pet-store/src/main/java/com/promineotech/pet/store/dao/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.promineotech.pet.store.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.promineotech.pet.store.entity.Employee;
-
-public interface EmployeeDao extends  JpaRepository<Employee, Long>  {
-
-}
--- Week-15-REST_API_/pet-store/src/main/java/com/promineotech/pet/store/dao/PetStoreDao.java
+++
@@ -1,8 +0,0 @@
-package com.promineotech.pet.store.dao;
-
-import com.promineotech.pet.store.entity.PetStore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetStoreDao extends JpaRepository<PetStore, Long> {
-
-}
